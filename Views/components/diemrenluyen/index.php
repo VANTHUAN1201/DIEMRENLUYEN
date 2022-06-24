7d464850a75419cd14e186184b0392b5cf94563c
Lực Nguyễn <kiemluc01@gmail.com>
2022-06-24 23:09:24 +0700

    vanthun mai iu bo

--- Views/components/diemrenluyen/index.php
+++ Views/components/diemrenluyen/index.php
@@ -9,54 +9,19 @@
       <div class="container-fluid">
           <div class="row mb-2">
               <div class="col-sm-6">
-                  <h1 class="m-0">Quản lí tiêu chí</h1>
+                  <h1 class="m-0"><strong>Trang chủ</strong></h1>
               </div>
           </div>
       </div>
   </div>
   <!-- main content  -->
-  <div class="card">
-    <div class="card-header">
-      <h3 class="card-title">Tiêu chí</h3>
-    </div>
-    <!-- /.card-header -->
-    <div class="card-body">
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th style="width: 10px">STT</th>
-            <th style="width:400px;">Danh mục</th>
-            <th>Các tiêu chí</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1.</td>
-            <td>Update software</td>
-            <td rowspan="4" style="overflow-y: auto!important;">
-              <table>
-                <tr>
-                  <td>ashd dadasd ask a dasdasd j adaskdsaa  adlaskdlsa  aldsad ad a d alsdlsak  asdaks da  á dkalsdla ashd dadasd ask a dasdasd j adaskdsaa  adlaskdlsa  aldsad ad a d alsdlsak  asdaks da  á dkalsdla ashd dadasd ask a dasdasd j adaskdsaa  adlaskdlsa  aldsad ad a d alsdlsak  asdaks da  á dkalsdla ashd dadasd ask a dasdasd j adaskdsaa  adlaskdlsa  aldsad ad a d alsdlsak  asdaks da  á dkalsdla </td>
-                </tr>
-              </table>
-            </td>
-          </tr>
-          <tr>
-            <td>2.</td>
-            <td>Clean database</td>
-          </tr>
-          <tr>
-            <td>3.</td>
-            <td>Cron job running</td>
-          </tr>
-          <tr>
-            <td>4.</td>
-            <td>Fix and squish bugs</td>
-          </tr>
-        </tbody>
-      </table>
+  <div class="card" >
+    <div style="display: flex!important;">
+      <img src="Public/images/logo.png" style="height:85vh; width:90vh;" alt="">
+      <h3 style="color: rgb(0, 53, 99);text-shadow:4px 4px 4px #000;font-size:90px;"><strong>Hệ thống chấm điểm rèn luyện cho sinh viên UTE</strong></h3>
     </div>
   </div>
+</div>
   <!-- main content  -->
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-latest.js"></script>
